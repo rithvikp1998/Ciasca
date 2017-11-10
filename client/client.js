1911d133d3410d16d04d583124e3b538018b4147
Rithvik Patibandla <rithvikp98@gmail.com>
2017-11-10 17:07:42 +0530

    Add some (untested) client side functions

--- client/client.js
+++ client/client.js
@@ -1,14 +1,31 @@
 var socket = io.connect('http://localhost:4000');
 
+var sidebar = document.getElementById('sidebar');
 var chatWindow = document.getElementById('chat-window');
 var inputField = document.getElementById('input-field');
 var sendButton = document.getElementById('send-button');
 
+function loadChannel(channelId){
+	socket.emit('requestChannelMessages', {
+		user: 'olaf',
+		channel: channelId
+	});
+	chatWindow.innerHTML = 'Loading...';
+}
+
 // Emit events
+window.addEventListener('load',function(){
+	socket.emit('requestUserSubscriptions', {
+		user: 'olaf'
+	});
+	sidebar.innerHTML = '<p>Loading...</p>';
+});
+
 sendButton.addEventListener('click', function(){
 	socket.emit('message', {
 		content: inputField.value,
-		user: 'olaf'
+		user: 'olaf',
+		timestamp: new Date()
 	});
 	inputField.value = '';
 });
@@ -16,5 +33,25 @@ sendButton.addEventListener('click', function(){
 
 // Listen for events
 socket.on('message', function(message){
-    chatWindow.innerHTML += '<p><strong>' + message.user + ': </strong>' + message.content + '</p>';
+    chatWindow.innerHTML += '<p><strong>' + message.user + ': </strong>' + message.content + ' ' + message.timestamp + '</p>';
+});
+
+socket.on('userSubscriptions', function(data){
+	sidebar.innerHTML = '';
+	for(var channel in data.subscriptions){
+		var channelId = channel.trim().toLower()
+		sidebar.innerHTML += '<p id=' + channelId + ' onClick=loadChannel(' + channelId + ')>' + channel + '</p>';
+	}
+});
+
+socket.on('channelMessages', function(data){
+	chatWindow.innerHTML = '';
+	var prevTimestamp = '';
+	for(var message in channelMessages){
+		if(message.timestamp.getDate()!=prevTimestamp){
+			chatWindow.innerHTML += '<p><strong> ' + message.timestamp.getDate() + ' </strong></p>';
+			prevTimestamp = message.timestamp.getDate();
+		}
+		chatWindow.innerHTML += '<p><strong>' + message.user + ': </strong>' + message.content + '</p>';
+	}
 });
